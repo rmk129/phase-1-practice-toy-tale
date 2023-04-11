4394d91bcb9c4a7a921a105e4d63296f00e0d941
Romil <romil.m.kothari@gmail.com>
2023-04-11 13:58:06 -0400

    tests completed

--- src/index.js
+++ src/index.js
@@ -13,3 +13,117 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:3000/toys") 
+    .then(function (resp) {
+      return resp.json()
+    })
+    .then(e => {
+      e.forEach(element => {
+       
+          let div = document.createElement("div");
+          div.className = "card";
+          let h2 = document.createElement("h2")
+          h2.innerHTML = element.name;
+          div.appendChild(h2);
+          let img = document.createElement("img")
+          img.src = element.image;
+          img.className = "toy-avatar"
+          div.appendChild(img)
+          let likes = document.createElement("p")
+          likes.innerText = `${element.likes} likes`
+          div.appendChild(likes);
+          let likeButton = document.createElement("button")
+          likeButton.className = "like-btn"
+          likeButton.id = element.id;
+          likeButton.innerHTML = "Like &hearts;"
+          likeButton.addEventListener('click', function(e) {
+            let newlikes = element.likes++;
+            likes.innerText = `${newlikes} likes!`
+            fetch(`http://localhost:3000/toys/${element.id}`, {
+              method: 'PATCH',
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+              },
+              body: JSON.stringify({
+                "likes": newlikes
+              })
+            })
+          })
+          div.appendChild(likeButton)
+          document.getElementById("toy-collection").appendChild(div)
+      });;
+    })
+
+})
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let name = e.target.tName
+    let image = e.target.tImage
+    fetch("http://localhost:3000/toys", {
+      method: 'POST', 
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "name": `${name.value}`,
+        "image": `${image.value}`,
+        "likes": 0
+      })
+    })
+    .then(resp => resp.json())
+    .then(element => {
+      let div = document.createElement("div");
+      div.className = "card";
+      let h2 = document.createElement("h2")
+      h2.innerHTML = element.name;
+      div.appendChild(h2);
+      let img = document.createElement("img")
+      img.src = element.image;
+      img.className = "toy-avatar"
+      div.appendChild(img)
+      let likes = document.createElement("p")
+      likes.innerText = `${element.likes} likes`
+      div.appendChild(likes);
+      let likeButton = document.createElement("button")
+      likeButton.className = "like-btn"
+      likeButton.id = element.id;
+      likeButton.innerHTML = "Like &hearts;"
+      likeButton.addEventListener('click', function(e) {
+        let newlikes = element.likes++;
+        likes.innerText = `${newlikes} likes!`
+        fetch(`http://localhost:3000/toys/${element.id}`, {
+          method: 'PATCH',
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify({
+            "likes": newlikes
+          })
+        })
+      })
+      div.appendChild(likeButton)
+      document.getElementById("toy-collection").appendChild(div)
+      
+    })
+  })
+})
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hearts = document.getElementsByClassName('like-btn')
+  console.log(hearts)
+
+//   for (const element of hearts){
+//     element.addEventListener('click', function(e) {
+//       console.log(e);
+//     })
+//   }
+})
